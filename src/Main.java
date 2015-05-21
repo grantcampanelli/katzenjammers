ddc80f00e5c054ed2c02073f3a370524860e31ca
Grant Campanelli <grantcampanelli@gmail.com>
2015-05-20 23:00:14 -0700

    Works locally with mysql installed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,36 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
+        Connection conn = null;
+        Statement stmt = null;
+        String name = null;
+        String gender = null;
+
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Katzenjammers","grant","Soccer57");
+            System.out.println("Hello World!");
+            System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT * FROM Source";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while(rs.next()) {
+                name = rs.getString("name");
+                gender = rs.getString("gender");
+                System.out.println("Name: "+name+" Gender: "+gender);
+            }
+        } catch (Exception ex) {
+            // handle the error
+            System.out.println("Error");
+        }
     }
 }
