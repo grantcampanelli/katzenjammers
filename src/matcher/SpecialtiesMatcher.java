bd997955290e4972cd3764e8c578e457b9737df8
Grant Campanelli <grantcampanelli@gmail.com>
2015-06-07 19:30:02 -0700

    Updated Specialties Matcher

--- src/matcher/SpecialtiesMatcher.java
+++ src/matcher/SpecialtiesMatcher.java
@@ -24,37 +24,22 @@ public class SpecialtiesMatcher implements Matcher{
                 sec1 = s1.secondarySpecialty, sec2 = s2.secondarySpecialty;
 
         if(prim1 == prim2) {
-            System.out.println("Same Primary Specialty");
+            //System.out.println("Same Primary Specialty");
             score = 1;
         }
 
         if(sec1 == sec2) {
-            System.out.println("Same Secondary Specialty");
+            //System.out.println("Same Secondary Specialty");
             score = 1;
         }
 
-        return score;
+        if(prim1 == sec2 || prim2 == sec1) {
+            //System.out.println("Same Primary Specialty as Other Secondary Specialty");
+            score = 0.5;
+        }
 
-//        String p1 = s1.phone, p2 = s2.phone;
-//        int diff = 0;
-//        if(p1.length() != PhoneLength || p2.length() != PhoneLength)
-//        {
-//            return score;
-//        }
-//        for(int index = 0; index < PhoneLength; index++)
-//        {
-//            if(p1.charAt(index) != p2.charAt(index))
-//            {
-//                diff++;
-//            }
-//        }
-//        System.out.println("Difference: " + diff);
-//        if(diff <= 3)
-//        {
-//            score = (double)(PhoneLength - diff) / PhoneLength;
-//        }
-//
-//        return score;
+        return score;
     }
+
 }
 
